bc5ec536e006b7b400be3880a5c833a66410ed43
Wathana <wathana.luct@gmail.com>
2021-12-30 15:31:51 +0700

    checking testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,10 @@
 import 'package:flutter/material.dart';
-//import 'package:xiaoming/pages/api_test2.dart';
-//import 'package:xiaoming/pages/lessons/aba_clone.dart';
 import 'package:xiaoming/pages/mptc_home_hr.dart';
 //import 'package:xiaoming/pages/mptc_signup_hr.dart';
 //import 'package:xiaoming/pages/mptc_forgetpassword_hr.dart';
 //import 'package:xiaoming/pages/mptc_dashboard.dart';
 //import 'package:xiaoming/pages/mptc_profile2.dart';
 //import 'package:xiaoming/pages/mptc_personal_info.dart';
-//import 'package:xiaoming/pages/template.dart';
-//import 'package:xiaoming/pages/ch00_appbar.dart';
-//import 'package:xiaoming/pages/ch05_widget_tree.dart';
-//import 'package:xiaoming/pages/ch05_widget_tree_performance.dart';
-//import 'package:xiaoming/pages/ch06_basic_widget.dart';
-//import 'package:xiaoming/pages/ch06_images.dart';
-//import 'package:xiaoming/pages/ch06_form_validation.dart';
-//import 'package:xiaoming/pages/ch06_orientation.dart';
-//import 'package:xiaoming/pages/ch07_animations.dart';
-//import 'package:xiaoming/pages/ch07_animation_controller.dart';
-//import 'package:xiaoming/pages/ch07_ac_staggered_animations.dart';
-//import 'package:xiaoming/pages/ch08_navigator.dart';
-//import 'package:xiaoming/pages/ch08_hero_animation.dart';
-//import 'package:xiaoming/pages/ch08_bottom_navigator_bar.dart';
-//import 'package:xiaoming/pages/ch08_bottom_app_bar.dart';
-//import 'package:xiaoming/pages/ch08_tabbar.dart';
-//import 'package:xiaoming/pages/ch08_drawer.dart';
-//import 'package:xiaoming/pages/ch09_listview.dart';
-//import 'package:xiaoming/pages/ch09_gridview.dart';
-//import 'package:xiaoming/pages/ch09_stack.dart';
-//import 'package:xiaoming/pages/ch09_customscrollview_slivers.dart';
-//import 'package:xiaoming/pages/ch11_gestures_drag_drop.dart';
-//import 'package:xiaoming/pages/ch11_dismissible.dart';
-//import 'package:xiaoming/pages/ch11_gestures_scale_book.dart';
-//import 'package:xiaoming/pages/ch12_platform_channel.dart';
-//import 'package:xiaoming/pages/ch13_local_persistence.dart';
-//import 'package:xiaoming/pages/ch14_journal.dart';
-//import 'package:xiaoming/pages/ming/test.dart';
-//import 'package:xiaoming/prius/mpg_kml.dart';
 void main() {
   runApp(MyApp());}
 
--- lib/services/authentication_api.dart
+++
@@ -1,9 +0,0 @@
-abstract class AuthenticationApi {
-  getFirebaseAuth();
-  Future<String> currentUserUid();
-  Future<void> signOut();
-  Future<String> signInWithEmailAndPassword({String email, String password});
-  Future<String> createUserWithEmailAndPassword({String email, String password});
-  Future<void> sendEmailVerification();
-  Future<bool> isEmailVerified();
-}
